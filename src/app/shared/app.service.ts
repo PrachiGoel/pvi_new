45b119c5b1a84627d005018effa4e36655149721
Prachi <prachi.goel@amdhan.co.in>
2021-09-07 15:30:19 +0530

    changes in table column dynamic

--- src/app/shared/app.service.ts
+++ src/app/shared/app.service.ts
@@ -6,7 +6,7 @@ import { HttpClient } from '@angular/common/http';
 export class AppService {
 
   constructor(private http:HttpClient) { }
-  REST_API_SERVER = "assets/data/dashboard.json";
+  REST_API_SERVER = "assets/data/itadmin.json";
   
   getDashboardData(){
     return this.http.get(this.REST_API_SERVER)
