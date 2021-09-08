a8f649d821563ce24298b75f462cfb0e74ef79a3
Prachi <prachi.goel@amdhan.co.in>
2021-09-08 14:08:21 +0530

    pagination added

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit,Component, OnInit,ViewChild } from '@angular/core';
 import { AppService } from 'src/app/shared/app.service';
-
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 @Component({
   selector: 'pvi-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  dataSource:any;
   dashboardData:any;
   displayedColumns:any[] = [];
-  dataSource : any[] = []
+  ELEMENT_DATA : any[] = [];
   constructor(private appService: AppService) { }
   
   fetchDashboardData(){
+     
     this.appService.getDashboardData().subscribe((data:any) => {
       console.log(data);
       this.dashboardData = data;
@@ -21,21 +27,27 @@ export class DashboardComponent implements OnInit {
         item = item.replace(/ /g, "");
         return item;
       });
+      this.displayedColumns.push("CaseHistory")
       let columns = this.dashboardData.searchResultList;
       columns.forEach((column:any)=>{
         var result = {} as any;
         this.displayedColumns.forEach((key,i) => {
-          result[key] = column[i];
+          if(key == 'CaseHistory'){
+            result[key] = "History"
+          }else{
+            result[key] = column[i];
+          }
         });
-        this.dataSource.push(result);
-        console.log(this.dataSource);
+        this.ELEMENT_DATA.push(result);
+        
       });
+      this.dataSource = new MatTableDataSource(this.ELEMENT_DATA);
+      this.dataSource.paginator = this.paginator;
     });
-    console.log(this.dashboardData)
   }
   
   ngOnInit(): void {
     this.fetchDashboardData()
   }
-
+ 
 }
